67697d2e15d6f0c0fc1a9d0524c332fff97312bf
Nahama <redsandsnake@gmail.com>
2020-11-27 18:05:54 +0900

    追加：チタンブロック

--- src/main/java/com/tntmodders/titaniummod/TitaniumMod.java
+++ src/main/java/com/tntmodders/titaniummod/TitaniumMod.java
@@ -1,7 +1,13 @@
 package com.tntmodders.titaniummod;
 
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
@@ -15,11 +21,30 @@ public class TitaniumMod {
 
     public TitaniumMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        Blocks.register(modEventBus);
         Items.register(modEventBus);
+    }
+
+    public static class Blocks {
+        private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
+        public static final RegistryObject<Block> TITANIUM_BLOCK = BLOCKS.register("titanium_block", () -> new Block(AbstractBlock.Properties
+                .create(Material.IRON)
+                .setRequiresTool()
+                .hardnessAndResistance(5.0F, 6.0F)
+                .sound(SoundType.METAL)
+                .harvestTool(ToolType.PICKAXE)
+                .harvestLevel(1)
+        ));
+
+        public static void register(IEventBus eventBus) {
+            BLOCKS.register(eventBus);
+        }
     }
 
     public static class Items {
         private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+        public static final RegistryObject<Item> TITANIUM_BLOCK = ITEMS.register("titanium_block", () -> new BlockItem(Blocks.TITANIUM_BLOCK.get(), (new Item.Properties())
+                .group(ItemGroup.BUILDING_BLOCKS)));
         public static final RegistryObject<Item> TITANIUM_INGOT = ITEMS.register("titanium_ingot", () -> new Item(new Item.Properties()
                 .group(ItemGroup.MATERIALS)));
 
